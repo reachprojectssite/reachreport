8a3b479ed75108010970fab3b8ef9b1076d6336c
Von <von@vrtcl.ai>
2025-03-23 16:48:02 +0000

    Revert "Revert "Revert "Optimized website performance with targeted improvements to loading speed and component rendering"""
    This reverts commit 42baaa7fc9114dd907e565ab18872cf54fe45c07.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -13,11 +13,7 @@ export default function RootLayout({ children }) {
           data-beehiiv-publication-id="pub_32491422-c94a-40b2-baec-c90cbb498271"
           src="https://embed.beehiiv.com/subscribe.js"
           async
-          defer
         />
-        <link rel="preconnect" href="https://www.bu.edu" />
-        <link rel="dns-prefetch" href="https://www.bu.edu" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
         {children}
