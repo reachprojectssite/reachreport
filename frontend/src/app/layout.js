d541a8331256d172e663dafe39aaa39c20740756
Von <von@vrtcl.ai>
2025-03-23 02:09:18 +0000

    Revert "Revert "Revert "Revert "Updated Beehiiv integration with custom subscribe button and improved script loading""""
    This reverts commit a27a2b7e66d06b25b2d4a9ba6aa0ab8a10350f84.

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono", 
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: 'The REACH Report',
@@ -21,24 +10,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script 
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w, d, s, id, a, h) {
-                w.beehiivObject = h;
-                w[h] = w[h] || function() {
-                  (w[h].q = w[h].q || []).push(arguments)
-                };
-                w[h].l = 1 * new Date();
-                a = d.createElement(s);
-                a.async = 1;
-                a.src = id;
-                document.head.appendChild(a);
-              })(window, document, 'script', 'https://dash.beehiiv.com/v1/api.js', {}, 'beehiiv');
-            `
-          }}
+          data-beehiiv-publication-id="pub_32491422-c94a-40b2-baec-c90cbb498271"
+          src="https://embed.beehiiv.com/subscribe.js"
+          async
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         {children}
       </body>
     </html>
